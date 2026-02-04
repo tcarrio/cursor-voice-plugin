a4e6cfde38f0b62e934f399ec0f101a946429f20
Tom Carrio <thomas.carrio@skillshare.com>
2026-02-04 18:17:29 -0500

    build: app runner for python tests in flake.nix

--- flake.nix
+++ flake.nix
@@ -7,10 +7,33 @@
     systems.url = "github:nix-systems/default";
   };
 
-  outputs = inputs: inputs.blueprint {
-    inherit inputs;
-    prefix = "nix/";
-  } // {
-    homeManagerModules.default = inputs.self.homeModules.voice-plugin-cursor;
-  };
+  outputs = inputs:
+    let
+      bp = inputs.blueprint {
+        inherit inputs;
+        prefix = "nix/";
+      };
+      systems = import inputs.systems;
+      lib = (import inputs.nixpkgs { system = builtins.head systems; }).lib;
+      apps = lib.genAttrs systems (system:
+        let
+          pkgs = import inputs.nixpkgs { inherit system; };
+          flake = inputs.self;
+        in
+        {
+          test-python = {
+            type = "app";
+            program = "${pkgs.writeShellScript "test-python" ''
+              set -e
+              cd "${flake}"
+              export PYTHONPATH="${flake}"
+              exec ${pkgs.python3}/bin/python -m unittest discover -s tests -p 'test_*.py' -v
+            ''}";
+          };
+        });
+    in
+    bp // {
+      homeManagerModules.default = inputs.self.homeModules.voice-plugin-cursor;
+      inherit apps;
+    };
 }
